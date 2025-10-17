a4cae7b20fe3719e67fc94c7465e79b77501ab55
Furkan Safa Altunyuva <103141929+fsaltunyuva@users.noreply.github.com>
2025-10-17 21:06:59 +0300

    movement and follow camera

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    private Vector2 _movement;
+    [SerializeField] private float speed;
+    
+    //TODO: Interact with objects in the game world
+    
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        _movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    void FixedUpdate()
+    {
+        _rb.velocity = _movement * speed * Time.fixedDeltaTime;
+    }
+}
