9e1e738593ab9166fcb5310616ac40a84414406e
Furkan Safa Altunyuva <103141929+fsaltunyuva@users.noreply.github.com>
2025-10-17 21:51:23 +0300

    currency system, TMP

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -5,6 +6,8 @@ public class Player : MonoBehaviour
     private Rigidbody2D _rb;
     private Vector2 _movement;
     [SerializeField] private float speed;
+    [SerializeField] public int currency;
+    [SerializeField] TextMeshProUGUI currencyText;
     
     //TODO: Interact with objects in the game world
     
@@ -21,5 +24,22 @@ public class Player : MonoBehaviour
     void FixedUpdate()
     {
         _rb.velocity = _movement * speed * Time.fixedDeltaTime;
+    }
+    
+    public void AddCurrency(int amount)
+    {
+        currency += amount;
+        currencyText.text = currency.ToString();
+    }
+    
+    public bool SpendCurrency(int amount)
+    {
+        if (currency >= amount)
+        {
+            currency -= amount;
+            currencyText.text = currency.ToString();
+            return true;
+        }
+        return false;
     }
 }
